feb9cf4079b63a3b98ec5ccbfa971212d9d18b28
Ryo Anazawa <anazawa@cpan.org>
2014-07-15 18:28:28 +0900

    define schema

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -1,4 +1,31 @@
-CREATE TABLE IF NOT EXISTS member (
-    id           INTEGER NOT NULL PRIMARY KEY,
-    name         VARCHAR(255)
+CREATE TABLE resources (
+              uri TEXT
+                  NOT NULL,
+     request_date TEXT -- YYYY-MM-DD hh:mm:ss
+                  NOT NULL,
+      status_code TEXT -- 200, 301, 404, ...
+                  NOT NULL,
+    response_date TEXT -- YYYY-MM-DD hh:mm:ss
+                  DEFAULT "{unknown}"
+                  NOT NULL,
+    last_modified TEXT -- YYYY-MM-DD hh:mm:ss
+                  DEFAULT "{unknown}"
+                  NOT NULL,
+             etag TEXT
+                  DEFAULT "{unknown}"
+                  NOT NULL,
+          content TEXT -- JSON
+                  DEFAULT "{n/a}"
+                  NOT NULL,
+      insert_date TEXT -- YYYY-MM-DD hh:mm:ss
+                  NOT NULL,
+      update_date TEXT -- YYYY-MM-DD hh:mm:ss
+                  NOT NULL,
+       CONSTRAINT pk_resources
+          PRIMARY KEY (uri),
+       CONSTRAINT valid_update_date
+            CHECK (update_date >= insert_date),
+       CONSTRAINT valid_response_date
+            CHECK (response_date = "{unknown}"
+                      OR response_date >= request_date)
 );
