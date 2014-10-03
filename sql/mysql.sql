30662ba0e106b76335ae0b4ac1df540bff05f712
Ryo Anazawa <anazawa@cpan.org>
2014-10-03 10:31:31 +0900

    add MySQL schema

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,17 @@
-CREATE TABLE IF NOT EXISTS member (
-    id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name         VARCHAR(255)
+CREATE TABLE resources (
+        request_id VARCHAR(255)
+                   NOT NULL,
+       request_uri VARCHAR(255)
+                   NOT NULL,
+      request_date VARCHAR(20) -- YYYY-MM-DD hh:mm:ss
+                   NOT NULL,
+       status_code VARCHAR(20) -- 200, 301, 404, ...
+                   DEFAULT "UNKNOWN"
+                   NOT NULL,
+     response_date VARCHAR(20) -- YYYY-MM-DD hh:mm:ss
+                   DEFAULT "UNKNOWN"
+                   NOT NULL,
+           content MEDIUMTEXT,
+        CONSTRAINT pk_resources
+           PRIMARY KEY (request_id)
 );
